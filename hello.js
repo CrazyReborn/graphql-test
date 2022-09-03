5076adac4553005f81e778c1ccd9910d488965bc
CrazyReborn <vladkuzodin@gmail.com>
2022-09-03 10:06:00 +0200

    i know kung-fu

--- hello.js
+++ hello.js
@@ -1,28 +1,67 @@
-const express = require('express');
-const { buildSchema } = require('graphql');
-const { graphqlHTTP } = require('express-graphql');
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
+
   type Query {
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getMessage(id: ID!): Message
   }
 `);
 
-const rootValue = {
-  rollDice: ({numDice, numSides}) => {
-    var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-    }
-    return output;
+let fakeDatabase = {};
+
+class Message {
+  constructor(id, {content, author} ) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
   }
+}
+
+const root = {
+  getMessage: ({ id }) => {
+    if(!fakeDatabase[id]) {
+      throw new Error('No message with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: ({ input }) => {
+    const id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, fakeDatabase[id]);
+  },
+  updateMessage: ({ id, input }) => {
+    if(!fakeDatabase[id]) {
+      throw new Error('No message with id ' + id);
+    };
+
+    fakeDatabase[id] = input;
+    return new Message(id, fakeDatabase[id]);
+  },
 };
 
-const app = express();
+var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: rootValue,
+  rootValue: root,
   graphiql: true,
 }));
 app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql')
+console.log('Running a GraphQL API server at localhost:4000/graphql');
